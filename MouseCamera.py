2bd48ec4affd380fc7579f29009f639996a006e2
Ryan S <kobitoko@gmail.com>
2023-06-10 00:44:27 -0600

    Added Scrolling, and fixed some issues

--- MouseCamera.py
+++ MouseCamera.py
@@ -20,8 +20,9 @@ class MouseCamera:
                 running_mode = self.vision_running_mode.VIDEO)
         # pyautogui only supports primary monitor for now, it isn't reliable for the screen of a second monitor.
         self.screen_width, self.screen_height = pyautogui.size()
+        # Failsafe is thumbs down which causes this to quit.
         pyautogui.FAILSAFE = False
-        pyautogui.PAUSE = 0.01
+        pyautogui.PAUSE = 0.001
 
         self.running = True
         self.x = 0
@@ -32,12 +33,15 @@ class MouseCamera:
         self.right_mouse_active = False
         self.left_mouse_down = False
         self.right_mouse_down = False
+        self.scroll_active = False
         self.mouse_move = False
-        
-        # basically speed
+        self.show_webcam = False
+        # mouse move speed
         self.multiplier = 1.5
+        # scroll speed
+        self.scroll_multiplier = 2
         # pixels radius movement should ignore
-        self.threshold_move = 5
+        self.threshold_move = 10
 
     def set_webcam_index(self, index = 0):
         self.camera = cv2.VideoCapture(index)
@@ -47,15 +51,21 @@ class MouseCamera:
         results_exists = result and result.gestures and len(result.gestures) > 0 and len(result.gestures[0]) > 0
         if results_exists:
             # The canned gestures are category_name ["None", "Closed_Fist", "Open_Palm", "Pointing_Up", "Thumb_Down", "Thumb_Up", "Victory", "ILoveYou"]
-            print(result.gestures[0][0].category_name, result.gestures[0][0].score)
+            # ILoveYou is apparently thumb, index, and pinkie extended.
+            #print(result.gestures[0][0].category_name, result.gestures[0][0].score)
             match result.gestures[0][0].category_name:
                 case "Closed_Fist":
                     self.mouse_move = True
+                    self.scroll_active = False
                     self.reset_mouse_buttons()
                 case "Pointing_Up":
                     self.left_mouse_active = True
+                    self.scroll_active = False
                 case "Victory":
                     self.right_mouse_active = True
+                    self.scroll_active = False
+                case "Thumb_Up":
+                    self.scroll_active = True
                 case "Open_Palm" | "None":
                     self.reset_mouse_states()
                 case "Thumb_Down":
@@ -65,17 +75,10 @@ class MouseCamera:
                 case _:
                     print("process_result defaulted with case:", result.gestures[0][0].category_name)
             if self.any_mouse_state_active():
-                # Values from https://developers.google.com/mediapipe/solutions/vision/gesture_recognizer#hand_landmark_model_bundle
+                # Value from https://developers.google.com/mediapipe/solutions/vision/gesture_recognizer#hand_landmark_model_bundle
                 wrist = result.hand_landmarks[0][0]
-                index_knuckle = result.hand_landmarks[0][5]
-                pinkie_knuckle = result.hand_landmarks[0][17]
-                # the cursor will be roughly in the center of the palm
-                x = (wrist.x + index_knuckle.x + pinkie_knuckle.x) / 3.0
-                y = (wrist.y + index_knuckle.y + pinkie_knuckle.y) / 3.0
-
                 x = wrist.x
                 y = wrist.y
-
                 if self.last_x == -1 or self.last_y == -1:
                     # it was last reset, we don't count those last values
                     self.last_x = x
@@ -89,11 +92,12 @@ class MouseCamera:
             self.reset_mouse_states()
 
     def any_mouse_state_active(self):
-        return self.mouse_move or self.left_mouse_active or self.right_mouse_active
+        return self.mouse_move or self.left_mouse_active or self.right_mouse_active or self.scroll_active
 
     def reset_mouse_states(self):
         self.reset_mouse_buttons()
         self.mouse_move = False
+        self.scroll_active = False
         self.last_x = -1
         self.last_y = -1
 
@@ -119,7 +123,6 @@ class MouseCamera:
                 pyautogui.mouseUp(button="right")
                 self.right_mouse_down = False
         if self.any_mouse_state_active():
-            #self.screen_width, self.screen_height
             diff_x = self.x - self.last_x
             diff_y = self.y - self.last_y
             move_x = diff_x * self.screen_width * self.multiplier
@@ -128,13 +131,18 @@ class MouseCamera:
                 move_x = 0
             if abs(move_y) < self.threshold_move:
                 move_y = 0
-            pyautogui.move(move_x, move_y)
+            if self.scroll_active:
+                # Takes only int, otherwise it crashes.
+                scroll_clicks = int(round(move_y * self.scroll_multiplier))
+                pyautogui.scroll(scroll_clicks)
+            else:
+                pyautogui.move(move_x, move_y)
 
     def start(self):
         try:
             # Taken from https://developers.google.com/mediapipe/solutions/vision/gesture_recognizer/python
             with self.gesture_recognizer.create_from_options(self.options) as recognizer:
-                print("Hi", self.screen_width, self.screen_height)
+                print("Starting")
                 while(self.running):
                     ret, frame = self.camera.read()
                     # flip image horizontally
@@ -144,10 +152,11 @@ class MouseCamera:
                     result = recognizer.recognize_for_video(mp_image, current_time_ms)
                     self.process_result(result)
                     self.perform_actions()
-                    cv2.imshow('Webcam View', frame)
-                    # run for 16ms (16ms = 60fps) and stop if ESC is pressed.
-                    if cv2.waitKey(16) == 27:
-                        break
+                    if self.show_webcam:
+                        cv2.imshow('Webcam View', frame)
+                        # run for 16ms (16ms = 60fps) and stop if ESC is pressed.
+                        if cv2.waitKey(16) == 27:
+                            break
         finally:
             print("Good bye.")
             cv2.destroyAllWindows()
